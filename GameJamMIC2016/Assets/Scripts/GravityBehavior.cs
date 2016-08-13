e3d8226ee38380c17154898b99d5a83fa07a1f55
LewisErick <repos.zul@gmail.com>
2016-08-13 09:47:05 -0500

    Merge with gravity

---
+++ GameJamMIC2016/Assets/Scripts/GravityBehavior.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		GameObject[] sceneObjects = GameObject.FindObjectsOfType(typeof (GameObject));
+		List<GameObject> movableObjects =  new List<GameObject>;
+
+		foreach(object obj in sceneObjects)
+		{
+			if(obj.GetComponen<MovableComponent> != null)
+			{
+
+				movableObjects.Add(obj);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKeyDown("space"))
+		{
+			foreach (object obj in movableObjects)
+			{
+				obj.GetComponen<MovableComponent>.applyGravity();
+			}
+	}
+}
